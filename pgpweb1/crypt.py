4b07e15fa4e369df98b979d767399ea08c6cd8cd
HrnekCesky <adabab0909@gmail.com>
2026-08-18 21:04:58 +0200

    switched out the encryption code for the code from docs

--- pgpweb1/crypt.py
+++ pgpweb1/crypt.py
@@ -11,8 +11,8 @@ def encrypt_post():
     name = request.forms.get('name')
     text = request.forms.get('text')
     #get stuff 
-    public_key = pgpy.PGPKey.from_blob(requests.get(f"{BLOB_API_URL}/{name}.asc").text)
+    public_key = pgpy.PGPKey.from_blob(str(requests.get(f"{BLOB_API_URL}/{name}.asc").text))
     message = pgpy.PGPMessage.new(str(text))
     #encrypt text
-    out = public_key.pgpy.encrypt(message)
+    out = public_key.encrypt(message)
     return f"{out}"
