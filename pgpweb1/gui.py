d59a097f876862b9e7f53a7e3eeabb75cf1536ef
HrnekCesky <adabab0909@gmail.com>
2026-08-18 02:45:17 +0200

    added A /gui menu

--- pgpweb1/gui.py
+++ pgpweb1/gui.py
@@ -1,10 +1,24 @@
 from bottle import route, request, response, view
 from datetime import datetime
 
+@route('/gui')
+@route('/gui/')
+@view('layout-gui')
+def gui():
+    return dict(
+        title='PGP Web GUI',
+        body='''
+        <a href="/gui/get">Get Key</a><br>
+        <a href="/gui/add">Add Key</a><br>
+        <a href="/gui/rem">Remove Key</a><br>
+        '''
+        )
+
+
 @route('/gui/get')
 @route('/gui/get/')
 @view('api-frontend-getkey')
-def gui():
+def get():
     return dict(
         apipath='/api/getkey'
         )
@@ -12,7 +26,7 @@ def gui():
 @route('/gui/add')
 @route('/gui/add/')
 @view('api-frontend-addkey')
-def gui():
+def add():
     return dict(
         apipath='/api/addkey'
         )
@@ -20,7 +34,7 @@ def gui():
 @route('/gui/rem')
 @route('/gui/rem/')
 @view('api-frontend-remkey')
-def gui():
+def rem():
     return dict(
         apipath='/api/remkey'
         )
---
+++ pgpweb1/views/api-frontend.tpl
@@ -0,0 +1,9 @@
+% rebase('layout-gui.tpl', title=title, body=body)
+
+<div class="cont">
+    <h1>PGP Key Management</h1>
+    <h2>{{ title }}</h2>
+    <div class="form-group">
+        {{ body }}
+    </div>
+</div>
